cfc9ef30ce8dc201480843ca40f2ddb17e986def
manuelito <roca.delacruz.lui18@gmail.com>
2017-07-19 11:15:01 -0500

    Agregado contenido super relevante en mi tesis

--- src/AlritmoNuevo.java
+++ src/AlritmoNuevo.java
@@ -2,6 +2,20 @@
 public class AlritmoNuevo {
 	public static void main(String[] args){
 		System.out.println("Algoritmo nuevo");
+		
+		int i=0;
+		do
+		{
+			System.out.println("El numero es: "+(i+1));
+			i++;
+		}while(i<10);
+	}
+	
+	public static void imprimirTesis()
+	{
+		System.out.println("Blabla...... relleno!!");
+		System.out.println("Ma lata!.");
+		System.out.println("lataaaa....");
 	}
 
 }
